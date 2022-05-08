03b8563add161895eae72512b2ae7640fca42d83
lvneto <lauro.neto@irriga.net>
2022-05-08 12:05:03 -0300

    application finally working

--- App.tsx
+++ App.tsx
@@ -34,7 +34,7 @@ export default function App() {
        translucent
         />
 
-        <Widget/>
+      <Widget/>
     </View>
   );
 }
--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -7,12 +7,21 @@ export const styles = StyleSheet.create({
   button: {
     width: 48,
     height: 48,
-    border: 24,
+    borderRadius: 24,
     backgroundColor: theme.colors.brand,
     justifyContent: 'center',
     alignItems: 'center',
     position: 'absolute',
     right: 16,
     bottom: getBottomSpace() + 16,   
+  },
+  modal: {
+    backgroundColor: theme.colors.surface_primary,
+    paddingBottom: getBottomSpace() + 16
+  },
+  indicator: {
+    backgroundColor: theme.colors.text_primary,
+    width: 56,
+    padding: 0
   }
 });
